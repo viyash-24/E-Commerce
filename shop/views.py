72b364de72b05ef3c4b41f38c317602d242c179a
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-21 20:11:09 +0530

    register function is added

--- shop/views.py
+++ shop/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
           return render(request,"shop/index.html")
+
+def register(request):
+          return render(request,"shop/register.html")
