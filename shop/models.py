a05a4c91d85847e69b9dc52afa375898afab25a1
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-24 20:50:43 +0530

    product model is created

--- shop/models.py
+++ shop/models.py
@@ -12,9 +12,23 @@ class Catagory(models.Model):
           image=models.ImageField(upload_to=getFileName,null=True,blank=True)
           description=models.TextField(max_length=500,null=False,blank=False)
           status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
-          trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
           created_at=models.DateTimeField(auto_now_add=True)
           
 def __str__(self):
     return self.name
 
+class Product(models.Model):
+          catagory=models.ForeignKey(Catagory,on_delete=models.CASCADE)
+          name=models.CharField(max_length=150,null=False,blank=False)
+          vendor=models.CharField(max_length=150,null=False,blank=False)
+          product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+          quantity=models.IntegerField(null=False,blank=False)
+          original_price=models.FloatField(null=False,blank=False)
+          selling_price=models.FloatField(null=False,blank=False)          
+          description=models.TextField(max_length=500,null=False,blank=False)
+          status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
+          trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+          created_at=models.DateTimeField(auto_now_add=True)
+          
+def __str__(self):
+    return self.name
