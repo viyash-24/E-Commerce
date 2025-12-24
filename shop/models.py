2e0f78d8391029d20dcb8f97cfcfdc423e837f27
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-24 20:41:48 +0530

    Catagory model is created

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
+import os
 
-# Create your models here.
+def getFileName(request,filename):
+          now_time=datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
+          new_filename="%s%s"%(now_time,filename)
+          return os.path.join('uploads/',new_filename)
+
+class Catagory(models.Model):
+          name=models.CharField(max_length=150,null=False,blank=False)
+          image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+          description=models.TextField(max_length=500,null=False,blank=False)
+          status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
+          trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+          created_at=models.DateTimeField(auto_now_add=True)
+          
+def __str__(self):
+    return self.name
+
