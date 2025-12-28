dd4c772675cc227350fe8dd0e37319b862219fcb
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-28 18:42:17 +0530

    register def function user form is added

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,14 @@ def home(request):
           return render(request,"shop/index.html",{"products":products})
 
 def register(request):
-          return render(request,"shop/register.html")
+          form=CustomUserForm()
+          if request.method=='POST':
+            form=CustomUserForm(request.POST)
+            if form.is_valid():
+              form.save()
+              messages.success(request,"Registration Success You can Login Now..!")
+              return redirect('/login')
+          return render(request,"shop/register.html",{'form':form})
 
 def collections(request):
           catagory=Catagory.objects.filter(status=0)
