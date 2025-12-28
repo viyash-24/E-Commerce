63acac9fb5d56091f3400c4d60912b4f57b094e0
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-28 18:34:11 +0530

    userform password1 and password2 feilds are created

--- shop/form.py
+++ shop/form.py
@@ -6,7 +6,8 @@ from django import forms
 class CustomUserForm(UserCreationForm):
   username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter User Name'}))
   email=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Email Address'}))
-  
+  password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Your Password'}))
+  password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Confirm Password'}))
   class Meta:
     model=User
-    fields=['username','email']
+    fields=['username','email','password1','password2']
