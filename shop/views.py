c6da9322aaa997d82f433ff289b661f5d9a4cfc2
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-28 18:48:55 +0530

    add_to_cart def function created

--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,28 @@ def home(request):
           products=Product.objects.filter(trending=1)
           return render(request,"shop/index.html",{"products":products})
 
+def add_to_cart(request):
+   if request.headers.get('x-requested-with')=='XMLHttpRequest':
+    if request.user.is_authenticated:
+      data=json.load(request)
+      product_qty=data['product_qty']
+      product_id=data['pid']
+      #print(request.user.id)
+      product_status=Product.objects.get(id=product_id)
+      if product_status:
+        if Cart.objects.filter(user=request.user.id,product_id=product_id):
+          return JsonResponse({'status':'Product Already in Cart'}, status=200)
+        else:
+          if product_status.quantity>=product_qty:
+            Cart.objects.create(user=request.user,product_id=product_id,product_qty=product_qty)
+            return JsonResponse({'status':'Product Added to Cart'}, status=200)
+          else:
+            return JsonResponse({'status':'Product Stock Not Available'}, status=200)
+    else:
+      return JsonResponse({'status':'Login to Add Cart'}, status=200)
+   else:
+    return JsonResponse({'status':'Invalid Access'}, status=200)
+
 def logout_page(request):
   if request.user.is_authenticated:
     logout(request)
